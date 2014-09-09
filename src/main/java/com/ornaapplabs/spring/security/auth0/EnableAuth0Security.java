22bd2975e5693d248375e8bf23b26aa2823ecba0
David A. Ortega <dortega@me.com>
2014-09-08 19:41:38 -0700

    Fixed some minor bugs, and added warning for empty ClientId and/or ClientSecret

--- src/main/java/com/ornaapplabs/spring/security/auth0/EnableAuth0Security.java
+++ src/main/java/com/ornaapplabs/spring/security/auth0/EnableAuth0Security.java
@@ -5,6 +5,7 @@ package com.ornaapplabs.spring.security.auth0;
  */
 
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -12,6 +13,7 @@ import java.lang.annotation.Target;
 @Retention(value=java.lang.annotation.RetentionPolicy.RUNTIME)
 @Target(value={java.lang.annotation.ElementType.TYPE})
 @Import({Auth0SecurityConfiguration.class})
+@EnableWebSecurity
 public @interface EnableAuth0Security {
 
 }
